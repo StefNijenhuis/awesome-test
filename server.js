e89a77482824d018a0dfcd4c2df09c2ffe4fd6ed
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-09-03 19:24:42 +0200

    Starting server method #2

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+    app = express();
+var path = require('path');
+
+app.get('/', function(req, res) {
+
+  res.sendFile(path.join(__dirname + '/index.html'));
+
+});
+
+
+app.listen(1337);
+console.log('1337 is the magic port!');
