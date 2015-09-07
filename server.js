53915f0b4aea19eb317fc2bba2cc6741c29744ce
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-09-07 11:19:55 +0200

    Adding basic node info to server.js

--- server.js
+++ server.js
@@ -1,2 +1,46 @@
-console.log('THIS IS SUPPOSED TO SAY SOMETHING!')
-console.log('HELLO WORLD!')
+// CALL THE PACKAGES
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
+
+// APP CONFIGURATION
+// use body parser so information can be taken from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// configure app to handle CORS requests
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')  ;
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+
+// log all requests to the console
+app.user(morgan('dev'));
+
+// ROUTES
+// basic route for the home page
+app.get('/', function(req, res) {
+  res.send('Welcome to the home page!');
+})
+
+// get an instance of the express router
+var apiRouter = express.Router();
+
+// test route to make sure everything is working
+// accessed at GET http://localhost:8080/api
+apiRouter.get('/', function(req, res) {
+  res.json({ message: 'Hooray! Welcome to our api!'});
+});
+
+// REGISTER OUR ROUTES
+// all of the routes will be prefixed with /api
+app.use('/api', apiRouter);
+
+// START THE SERVER
+app.listen(port);
+console.log('Magic happens on part' + port);
