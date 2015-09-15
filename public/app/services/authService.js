f7f0d9a172ec52f4e1c02d2e58cfbd51ac2f63d2
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-09-15 13:16:27 +0200

    Adding auth and authinterceptor services

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -1,5 +1,55 @@
 angular.module('authService', [])
 
+// auth factory to login and get information
+// inject $http for communicating with the API
+// inject $q to return promise objects
+// inject AuthToken to manage tokens
+.factory('Auth', function($http, $q, AuthToken) {
+
+  // create auth factory object
+  var authFactory = {};
+
+  // log a user in
+  authFactory.login = function(username, password) {
+    // return the promise object and its data
+    return $http.post('/api/authenticate', {
+      username: username,
+      password: password
+    })
+      .success(function(data) {
+        authToken.setToken(data.token);
+        return data;
+      });
+  };
+
+  // log a user out by clearing the token
+  authFactory.logout = function() {
+    // clear the token
+    authToken.setToken();
+  };
+
+  // check if user is logged in
+  // checks if there is a local token
+  authFactory.isLoggedIn = function () {
+    if (AuthToken.getToken())
+      return true;
+    else
+      return false;
+  };
+
+  // get the logged in user
+  authFactory.getUser = function() {
+    if (authToken.getToken())
+      return $http.get('/api/me');
+    else
+      return $q.reject({ message: 'User has no token. '});
+  };
+
+  //return auth factory object
+  return authFactory;
+
+})
+
 // factory for handling tokens
 // inject $window to store token client-side
 .factory('AuthToken', function($window) {
@@ -26,35 +76,36 @@ angular.module('authService', [])
 
 })
 
-// auth factory to login and get information
-// inject $http for communicating with the API
-// inject $q to return promise objects
-// inject AuthToken to manage tokens
-.factory('Auth', function($http, $q, AuthToken) {
-
-  // create auth factory object
-  var authFactory = {};
-
-  // handle login
-
-  // handle logout
-
-  // check uf yser is logged in
-
-  // get the user info
-
-  //return auth factory object
-  return authFactory;
-
-})
-
 // application configuration to integrate token into requests
-.factory('AuthInterceptor', function($q, AuthToken) {
+.factory('AuthInterceptor', function($q, $location, AuthToken) {
 
   // create AuthInterceptor factory object
   var authInterceptorFactory = {};
 
-  // attach the token to every request
+  // this will happen on all HTTP requests
+  authInterceptorFactory.request = function(config) {
+
+    // grab the token
+    var token = authToken.getToken();
+
+    // if the token exists, add it to the header as x-access-token
+    if (token)
+      config.headers['x-access-token'] = token;
+    return config;
+  };
+
+  // happens on response errors
+  authInterceptorFactory.responseError = function(response) {
+
+    // if our server returns a 403 forbidden response
+    if (response.status == 403) {
+      AuthToken.setToken();
+      $location.path('/login');
+    }
+
+    // return the errors from the server as a promise
+    return $q.reject(response)
+  };
 
   // redirect if a token deosn't authenticate
 
