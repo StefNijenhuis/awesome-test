1002ddf91b4d15a4ae87af1159f8a98470325175
Stef Nijenhuis <admin@stefnijenhuis.nl>
2015-09-15 14:22:44 +0200

    Adding home.html, creating routes and the main controller for angular

---
+++ public/app/app.routes.js
@@ -0,0 +1,14 @@
+angular.module('app.rouyes', [ngRoute])
+
+.config(function($routeProvider, $locationProvider) {
+
+  $routeProvider
+
+    // home page route
+    .when('/', {
+      templateUrl : 'app/views/pages/home.html'
+    });
+
+    // get rid of the hash in URL
+    $locationProvider.html5Mode(true);
+});
